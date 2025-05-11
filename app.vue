8a98351de514543505a7a3d96d2e6c1d5e9ae3f3
Alex-Shkuropatov <a.shkuropatov.workbox@gmail.com>
2025-05-11 20:32:46 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+ <NuxtLoadingIndicator />
+ <ViewState v-slot="path" src="route://path">
+  <ViewState v-slot="page" :src="`app-config://usebootstrap/pages`" :path="path.data">
+   <div v-if="page.data">
+
+    <Head>
+     <PageMeta :title="page.data.title" :description="page.data.description" :route-meta="page.data">
+      <LdArticle :headline="page.data.title" />
+     </PageMeta>
+    </Head>
+    <NuxtLayout :name="page.data.layout">
+     <NuxtPage />
+    </NuxtLayout>
+   </div>
+   <NuxtPage v-else />
+  </ViewState>
+ </ViewState>
+</template>
